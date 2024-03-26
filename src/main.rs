2dca7639a6e11074cbbea1804b4babf2329af340
TeaShull <timshull99@icloud.com>
2024-03-26 14:32:08 -0500

    Fix generic file downloading structure

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,44 @@
-use reqwest::Error;
+use anyhow::Result;
 use scraper::{Html, Selector};
-use std::fs::File;
-use std::io::copy;
+use std::{fs::File, io::copy};
+use tokio::runtime::Runtime;
 
-async fn extract_image_urls(url: &str) -> Result<Vec<String>, Error> {
+async fn extract_image_urls(url: &str) -> Result<Vec<String>> {
     let response = reqwest::get(url).await?;
     let body = response.text().await?;
-    let html = Html::parse_document(&body);
+    let document = Html::parse_document(&body);
+    let image_selector = Selector::parse("img").expect("Error parsing image selector");
 
-    let image_selector = Selector::parse("img").unwrap();
-    let image_urls: Vec<String> = html
+    let base_url = "https://";
+
+    let image_urls: Vec<String> = document
         .select(&image_selector)
         .filter_map(|element| element.value().attr("src"))
-        .filter_map(|src| Some(src.to_owned()))
+        .map(|src| if src.starts_with("//") { format!("{}{}", base_url, &src[2..]) } else { src.to_string() })
         .collect();
 
     Ok(image_urls)
 }
 
-async fn fetch_webpage(url: &str) -> Result<(), Error> {
+async fn download_images(image_urls: &[String]) -> Result<()> {
+    for (index, image_url) in image_urls.iter().enumerate() {
+        let response = reqwest::get(image_url).await?;
+        let mut dest = File::create(format!("image_{}.jpg", index))?;
+        copy(&mut response.bytes().await?.as_ref(), &mut dest)?;
+    }
+    Ok(())
+}
+
+async fn fetch_webpage(url: &str) -> Result<()> {
     let image_urls = extract_image_urls(url).await?;
     println!("Image URLs: {:?}", image_urls);
     download_images(&image_urls).await?;
     Ok(())
 }
 
-async fn download_images(image_urls: &[String]) -> Result<(), Error> {
-    for (index, image_url) in image_urls.iter().enumerate() {
-        let response = reqwest::get(image_url).await?;
-        let mut dest = File::create(format!("image_{}.jpg", index))
-            .map_err(|err| err.into())?;
-        copy(&mut response.bytes().await?.as_ref(), &mut dest)
-            .map_err(|err| err.into())?;
-    }
+fn main() -> Result<()> {
+    let url = ""; 
+    Runtime::new()?.block_on(fetch_webpage(url))?;
     Ok(())
 }
 
-fn main() {
-    let url = "";
-    tokio::runtime::Runtime::new().unwrap().block_on(fetch_webpage(url)).unwrap();
-}
